5e0c471829cd0475313a2dd9ba31e19134dee813
Fudge <chimsoo32@gmail.com>
2026-04-19 17:37:17 +0800

    feat: add authentication routes to the Express app

--- index.js
+++ index.js
@@ -2,12 +2,14 @@ require('dns').setServers(['8.8.8.8', '8.8.4.4']);
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
+const authRoutes = require('./API/auth');
 require('dotenv').config();
 
 const app = express();
 
 app.use(cors());
 app.use(express.json()); 
+app.use("/auth", authRoutes);
 
 mongoose
   .connect(process.env.MONGO_URI)
