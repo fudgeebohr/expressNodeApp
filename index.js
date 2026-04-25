3be70d682dc17882c43c29428de4185e660b14e6
Fudge <chimsoo32@gmail.com>
2026-04-25 22:46:28 +0800

    feat: add options route for handling Options model

--- index.js
+++ index.js
@@ -4,12 +4,14 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 const authRoutes = require('./API/auth');
 require('dotenv').config();
+const optionsRoute = require("./models/Options");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json()); 
 app.use("/auth", authRoutes);
+app.use("/options", optionsRoute);
 
 mongoose
   .connect(process.env.MONGO_URI)
